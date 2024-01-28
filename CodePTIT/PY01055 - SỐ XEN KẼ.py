3651e9fdcc84fbea5b106d5f9d39b1bb589b88ac
HailamAICT <hailam04.work@gmail.com>
2024-01-29 00:45:56 +0700

    29/1/2024

--- CodePTIT/PY01055 - SỐ XEN KẼ.py
+++ CodePTIT/PY01055 - SỐ XEN KẼ.py
@@ -1,7 +1,7 @@
 def check(n):
     if len(n)%2==0: return "NO"
-    if n[0]==n[1]: return "NO"
-    for i in range(2,len(n)):
+    if n[0]==n[1] or n[0]!=n[len(n)-1]: return "NO"
+    for i in range(2,len(n),2):
         if n[i]!=n[0]:
             return "NO"
     return "YES"
