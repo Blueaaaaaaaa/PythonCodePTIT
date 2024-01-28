14f20923ae6c9581afc59c79f7ce39ad73659a78
HailamAICT <hailam04.work@gmail.com>
2024-01-28 19:53:37 +0700

    28/1/2024

--- CodePTIT/PY01038 - KIỂM TRA CHIA HẾT CHO 7.py
+++ CodePTIT/PY01038 - KIỂM TRA CHIA HẾT CHO 7.py
@@ -1,15 +1,6 @@
-def rev(n):
-    n=str(n)
-    n=n[::-1]
-    return int(n)
 def check(n):
-    for i in range(1000):
-        if (n+rev(n))%7==0:
-            return n+rev(n)
-        else:
-            n=n+rev(n)
-    return -1
+    for i in range(len(n)//2):
+        if()
 for test in range(int(input())):
     n=int(input())
-    if n%7==0: print(n)
-    else: print(check(n))
+    print(check(n))
