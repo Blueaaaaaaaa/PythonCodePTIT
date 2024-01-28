cfe0f43edfb59393f7a35a32e6b94997783a0827
HailamAICT <hailam04.work@gmail.com>
2024-01-29 00:34:52 +0700

    29/1/2024

---
+++ CodePTIT/PY01052 - TỔNG CHỮ SỐ NGUYÊN TỐ.py
@@ -0,0 +1,19 @@
+from math import *
+def isum(n):
+    sum=0
+    for i in range(len(n)):
+        sum+=int(n[i])
+    return sum
+def isprimes(n):
+    if n<2: return "NO"
+    for i in range(2,int(sqrt(n))+1):
+        if n % i == 0:
+            return "NO"
+    return "YES"
+
+
+
+for test in range(int(input())):
+    n=input()
+    print(isprimes(isum(n)))
+
