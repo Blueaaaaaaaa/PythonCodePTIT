af49253b589d22bf96c2548b3123a5473425f4af
HailamAICT <hailam04.work@gmail.com>
2024-02-17 17:58:32 +0700

    17/2/2024

---
+++ CodePTIT/PY02024 - SẮP XẾP THEO TÍCH CHỮ SỐ.py
@@ -0,0 +1,14 @@
+def sumof(n):
+    sum=1
+    while n:
+        sum*=(n%10)
+        n//=10
+    return sum
+for i in range(int(input())):
+    n=int(input())
+    a=list(map(int,input().split()))
+    a=sorted(a)
+    a=sorted(a,key=sumof)
+    for i in a:
+        print(i,end=" ")
+    print()
