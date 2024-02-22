fd29323d0161e1d784115dd71243630861b17d83
HailamAICT <hailam04.work@gmail.com>
2024-02-22 23:20:06 +0700

    22/2/2024

--- CodePTIT/PYKT041 - ĐẾM CẶP ĐỒNG XU.py
+++ CodePTIT/PYKT041 - ĐẾM CẶP ĐỒNG XU.py
@@ -1,20 +1,16 @@
-from math import *
 n=int(input())
-a=[input() for j in range(n)]
-b=[0]*n
-count=0
-for i in a:
-    ans=0
-    for j in range(n):
-        if i[j] == 'C':
-            ans+=1
-    if ans>=2:
-        count+=comb(ans,2)
-for i in range(n):
-    for j in range(n):
-        if a[j][i]=='C':
-            b[i]+=1
-for i in b:
-    if i>=2:
-        count+=comb(i,2)
-print(count)
+a=[input() for i in range(n)]
+print(a)
+# sit=0
+# count=[]
+# while sit<n:
+#     if len(a[sit].split())==6:
+#         while sit<n and len(a[sit].split())==6:
+#             sit+=2
+#         a.append(1)
+#
+#     elif len(a)>=4 && len(a[sit].split())==7:
+#         count.append(2)
+#         sit+=4
+# print(len(count))
+# print(*count)
---
+++ CodePTIT/PYKT073 - XÁC ĐỊNH THỂ LOẠI THƠ.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=[input() for i in range(n)]
+sit=0
+count=[]
+while sit<n:
+    if len(a[sit].split())==6:
+        while sit<n and len(a[sit].split())==6:
+            sit+=2
+        count.append(1)
+    elif len(a)>=4 and len(a[sit].split())==7:
+        count.append(2)
+        sit+=4
+print(len(count))
+for i in count: print(i)
