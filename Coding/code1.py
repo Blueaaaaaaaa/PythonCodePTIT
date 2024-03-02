e5ae4ac2e042551a7e15029409ae66d9bcd2dc63
HailamAICT <hailam04.work@gmail.com>
2024-03-02 14:38:51 +0700

    add

--- Coding/code1.py
+++ Coding/code1.py
@@ -1,6 +1,23 @@
-input()
-a = {int(i) for i in input().split()}
-b = {int(i) for i in input().split()}
-print(*sorted(a & b))
-print(*sorted(a - b))
-print(*sorted(b - a))
+tmp=[0]*1001
+primes=[]
+def sieve():
+    for i in range(2,1001):
+        if tmp[i]==0:
+            primes.append(i)
+        for j in range(i*i,1001,i):
+            tmp[j]=1
+sieve()
+n=int(input())
+a=[int(x) for x in input().split()]
+res=[]
+for i in a:
+    if i in primes:
+        res.append(i)
+res=sorted(res)
+temp=0
+for i in a:
+    if i not in primes:
+        print(i,end=" ")
+    else:
+        print(res[temp],end=" ")
+        temp+=1
--- Coding/code2.py
+++ Coding/code2.py
@@ -1,19 +1,19 @@
-t = int(input())
-for i in range(t) :
-    n, m, k = [int(x) for x in input().split()]
-    p1, p2, p3, ok = 0, 0, 0, 0
-    a = [int(x) for x in input().split()]
-    b = [int(x) for x in input().split()]
-    c = [int(x) for x in input().split()]
-    while p1 < n and p2 < m and p3 < k :
-        if a[p1] == b[p2] and b[p2] == c[p3] :
-            print(a[p1], end = ' ')
-            ok = 1
-            p1 += 1
-            p2 += 1
-            p3 += 1
-        elif a[p1] < b[p2] : p1 += 1
-        elif b[p2] < c[p3] : p2 += 1
-        elif c[p3] < a[p1] : p3 += 1
-    if ok == 0 : print('NO')
-    else : print()
+rag_prompt_default_vi = f"""
+Bạn là một chuyên gia pháp luật và người giải quyết vấn đề, được giao nhiệm vụ trả lời mọi câu hỏi về Luật Đấu thầu.
+
+Cần phát triển một câu trả lời toàn diện và thông tin, không quá 80 từ, cho câu hỏi đã cho dựa trên các thông tin và nguyên tắc pháp lý có liên quan: {context}. 
+Câu hỏi cần trả lời: {query}.
+Bạn cần tuân thủ format sau trong việc trả lời:
+Trong trường hợp không có thông tin đầy đủ để trả lời, hãy cung cấp thông tin mà bạn đã biết hoặc chỉ dẫn đến nguồn thông tin có liên quan. Điều này giúp đảm bảo rằng câu trả lời luôn hữu ích và chính xác theo ngữ cảnh của luật đấu thầu.
+
+Câu Hỏi : "Điều kiện ký kết hợp đồng với nhà thầu theo Luật Đấu thầu 2023 là gì?", 
+Trả Lời  : "Theo Điều 69 Luật Đấu thầu 2023, nguyên tắc thực hiện hợp đồng với nhà thầu bao gồm: [Trách nhiệm của các bên], [Bảo đảm trung thực], [Tuân thủ pháp luật], [Bảo vệ lợi ích Nhà nước và cộng đồng]."
+Câu Hỏi: "Giá trị đảm bảo dự thầu theo Luật Đấu thầu 2023 là từ bao nhiêu đến bao nhiêu %", 
+Trả Lời: "Tỷ lệ đảm bảo dự thầu theo Luật Đấu thầu 2023 là từ 1% đến 3%, phụ thuộc vào loại gói thầu và giá trị của nó, áp dụng cho các gói thầu xây lắp, hỗn hợp và mua sắm hàng hóa."
+Câu Hỏi: "Có bao nhiêu phương thức lựa chọn nhà thầu theo Luật Đấu thầu 2023?", 
+Trả Lời: "Luật Đấu thầu 2023 quy định 9 phương thức lựa chọn nhà thầu, bao gồm: [Đấu thầu rộng rãi], [Đấu thầu hạn chế], [Chỉ định thầu], [Chào hàng cạnh tranh], [Mua sắm trực tiếp], [Tự thực hiện], [Tham gia của cộng đồng], [Đàm phán giá], [Trường hợp đặc biệt]."
+Câu Hỏi: <context>
+    {query} 
+<context/>
+Trả Lời: Suy nghĩ theo từng bước và trả lời.
+"""
