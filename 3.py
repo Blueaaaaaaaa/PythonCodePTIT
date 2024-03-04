11d2d59bfd85ff3761523be516a2f5e019342442
HailamAICT <hailam04.work@gmail.vom>
2024-03-04 21:20:12 +0700

    04/03/2024

--- 3.py
+++ 3.py
@@ -1,17 +1,15 @@
-Cho dãy số A[] có N phần tử là các số nguyên dương không quá 1000. Sau khi loại bỏ tất cả các giá trị bị lặp lại ở trong A[] ta tạo được dãy B[] có m phần tử là các giá trị khác nhau theo đúng thứ tự xuất hiện trong dãy A[].
+Cho một dãy ký tự số không quá 1000 chữ số và không có chữ số 0.
 
-Hãy tìm vị trí i nhỏ nhất (tính từ 0) trong dãy B[] thỏa mãn:
+Người ta lần lượt lấy ra mỗi lần 2 chữ số tính từ trái sang phải. Nếu bước cuối cùng không đủ hai chữ số thì bỏ qua chữ số đó. 
 
-Tổng các phần tử từ B[0] đến B[i] là một số nguyên tố
-Tổng các phần tử từ B[i+1] đến B[m-1] cũng là một số nguyên tố.
+Kết quả sẽ được một dãy số nguyên dương A[] chỉ bao gồm các số có hai chữ số.
+
+Hãy liệt kê các số khác nhau xuất hiện trong A[] theo thứ tự tăng dần.
+
 Input
 
-Dòng đầu ghi số N (1 < N < 500).
-
-Dòng tiếp theo ghi N số của dãy A[]
+Chỉ có một dòng ghi dãy ký tự số (độ dài không quá 1000). Dữ liệu vào đảm bảo không có chữ số 0.
 
 Output
 
-Ghi ra vị trí i đầu tiên tìm được.
-
-Nếu không có vị trí thỏa mãn thì ghi ra dòng chữ NOT FOUND
+Ghi ra lần lượt các số khác nhau xuất hiện trong dãy A[] theo thứ tự tăng dần, mỗi số viết cách nhau một khoảng trống.
