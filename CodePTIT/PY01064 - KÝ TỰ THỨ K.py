99301bd719ce1f9b09ca234f59d88e31022e3208
HailamAICT <hailam04.work@gmail.vom>
2024-03-05 13:50:38 +0700

    05/03/2024

--- CodePTIT/PY01064 - KÝ TỰ THỨ K.py
+++
@@ -1,7 +0,0 @@
-def find(n,k):
-    if k==2**(n-1): return n
-    elif k>2**(n-1): return find(n-1,k-2**(n-1))
-    return find(n-1,k)
-for test in range(int(input())) :
-    n, k = [int(x) for x in input().split()]
-    print(chr(find(n,k)+ord('A')-1))
