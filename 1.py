445eae5591fd76b5ba47d67726a64cd5d1ffbfd0
HailamAICT <hailam04.work@gmail.vom>
2024-03-06 09:05:47 +0700

    06/03/2024

--- 1.py
+++ 1.py
@@ -1,9 +1,2 @@
-n=int(input())
-a = sorted([int(x) for x in input().split()])
-res=[]
-res = []
-res.append(a[0] * a[1])
-res.append(a[0] * a[1] * a[n - 1])
-res.append(a[n - 3] * a[n - 2] * a[n - 1])
-res.append(a[n - 2] * a[n - 1])
-print(max(res))
+PY02066 - BÀI TOÁN ĐẾM
+
