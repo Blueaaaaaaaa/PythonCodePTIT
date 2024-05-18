59ac373ef43acbd4a9fb05bd69a0d4441e43b328
HailamAICT <hailam04.work@gmail.com>
2024-05-18 14:39:20 +0700

    Review

--- 2.py
+++ 2.py
@@ -1,18 +1,47 @@
-import os
-import json
-print(os.getcwd())
+import math
 
-# print(os.path.join("CodePTIT"))
-dir=os.getcwd()
-i=0
-# for files in os.listdir('CodePTIT'):
-#     file_path=os.path.join('CodePTIT',files)
-#     with open(file_path,'r') as file:
-#         for line in file:
-#             print(line,i,end=" ")
-#             print()
-#             i+=1
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-with open("questions_answers.json",'r') as file:
-    content=json.load(file)
-    print(content[1]['question'])
+class Triangle:
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+    def side_length(self, point1, point2):
+        return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+
+    def is_valid_triangle(self):
+        a = self.side_length(self.p1, self.p2)
+        b = self.side_length(self.p2, self.p3)
+        c = self.side_length(self.p3, self.p1)
+        return (a + b > c) and (a + c > b) and (b + c > a)
+
+    def perimeter(self):
+        if not self.is_valid_triangle():
+            return "INVALID"
+        a = self.side_length(self.p1, self.p2)
+        b = self.side_length(self.p2, self.p3)
+        c = self.side_length(self.p3, self.p1)
+        return round(a + b + c, 3)
+
+def main():
+    n = int(input().strip())
+    results = []
+    for _ in range(n):
+        x1, y1, x2, y2, x3, y3 = map(float, input().strip().split())
+        p1 = Point(x1, y1)
+        p2 = Point(x2, y2)
+        p3 = Point(x3, y3)
+        triangle = Triangle(p1, p2, p3)
+        result = triangle.perimeter()
+        results.append(result)
+
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
